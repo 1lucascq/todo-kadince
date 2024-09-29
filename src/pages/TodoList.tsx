c74cbcfba16066ea268366b1ad084556d054f380
lucas.quadros <1lucascq@gmail.com.br>
2024-09-29 19:07:43 -0300

    feat: add todo list and todo list items

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Todo, Filter } from '../lib/types';
 import TodoForm from '../components/TodoForm';
+import TodoListItems from '../components/TodoListItems';
+import FilterButtons from '../components/FilterButtons';
 
 const App: React.FC = () => {
     const [todos, setTodos] = useState<Todo[]>([]);
@@ -23,12 +25,35 @@ const App: React.FC = () => {
         setTodos([newTodo, ...todos]);
     }
 
+    function deleteTodo(id: number) {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    }
+
+    function editTodo(id: number, newText: string) {
+        setTodos(
+            todos.map((todo) =>
+                todo.id === id ? { ...todo, text: newText } : todo
+            )
+        );
+    }
+
     return (
         <main className="container max-w-xl mx-auto p-4 dark">
             <h1 className="text-2xl my-5 font-bold text-center text-kGreen">
-            	To-Dos
+                To-Dos
             </h1>
             <TodoForm addTodo={addTodo} />
+            <FilterButtons
+                activeFilter={activeFilter}
+                setActiveFilter={setActiveFilter}
+            />
+            <TodoListItems
+                todos={todos}
+                activeFilter={activeFilter}
+                toggleComplete={toggleComplete}
+                editTodo={editTodo}
+                deleteTodo={deleteTodo}
+            />
         </main>
     );
 };
