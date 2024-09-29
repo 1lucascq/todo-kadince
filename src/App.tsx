5f69c1d33d2e8429dca2963e2cac2657ca4e165d
lucas.quadros <1lucascq@gmail.com.br>
2024-09-29 14:04:11 -0300

    setup: update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
-
-function App() {
-  return (
-    <div className="">
-      <header className="">
-        <p className='text-center text-3xl font-bold'>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
-}
+import TodoList from './pages/TodoList';
+	
+const App: React.FC = () => {
+    return <TodoList />
+};
 
 export default App;
