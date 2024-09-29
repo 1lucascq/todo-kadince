16d741b2c1eefe719539e1df51ef30a3680c1b99
lucas.quadros <1lucascq@gmail.com.br>
2024-09-29 19:04:16 -0300

    feat: todo list input

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -4,20 +4,30 @@ import TodoForm from '../components/TodoForm';
 
 const App: React.FC = () => {
     const [todos, setTodos] = useState<Todo[]>([]);
-    // const [activeFilter, setActiveFilter] = useState<Filter>('All');
+    const [activeFilter, setActiveFilter] = useState<Filter>('All');
 
-    const addTodo = (text: string) => {
+    function toggleComplete(id: number) {
+        setTodos(
+            todos.map((todo) =>
+                todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            )
+        );
+    }
+
+    function addTodo(text: string) {
         const newTodo: Todo = {
             id: Date.now(),
             text,
-            status: 'Pending',
+            completed: false,
         };
         setTodos([newTodo, ...todos]);
-    };
+    }
 
     return (
-        <main className="container max-w-lg mx-auto p-4 dark">
-            <h1 className="text-2xl font-bold mb-4 text-center">Kadince To-Do's</h1>
+        <main className="container max-w-xl mx-auto p-4 dark">
+            <h1 className="text-2xl my-5 font-bold text-center text-kGreen">
+            	To-Dos
+            </h1>
             <TodoForm addTodo={addTodo} />
         </main>
     );
