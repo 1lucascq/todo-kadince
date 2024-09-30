58ff4b1a9a25726661abe2f79eb0d12185354e11
lucas.quadros <1lucascq@gmail.com.br>
2024-09-30 16:02:05 -0300

    chore: fix typo and improve code readability

--- src/components/FilterButtons.tsx
+++ src/components/FilterButtons.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 import { IFilterButtonsProps } from '../lib/types';
 import { FILTERS } from '../lib/helpers';
 
-const FilterButtons: React.FC<IFilterButtonsProps> = ({ activeFilter, setActiveFilter }) => {;
+const FilterButtons: React.FC<IFilterButtonsProps> = ({
+    activeFilter,
+    setActiveFilter,
+}) => {
     return (
         <div className="mb-4 flex justify-center">
-            {FILTERS.map(filter => (
+            {FILTERS.map((filter) => (
                 <button
                     key={filter}
-                    onClick={() =>
-                        setActiveFilter(filter)
-                    }
+                    onClick={() => setActiveFilter(filter)}
                     className={`px-4 py-2 mr-2 rounded ${
-                        activeFilter === filter ? 'bg-kGreen text-kBlack' : 'bg-gray-200 text-kDrakGreen'
+                        activeFilter === filter
+                            ? 'bg-kGreen text-kBlack'
+                            : 'bg-gray-200 text-kDarkGreen'
                     }`}
-					data-testid={`${filter}-filter`}
+                    data-testid={`${filter}-filter`}
                 >
                     {filter}
                 </button>
