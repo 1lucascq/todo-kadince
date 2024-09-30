0e10384c2aeda076863faa4e29176360469b4445
lucas.quadros <1lucascq@gmail.com.br>
2024-09-30 16:03:04 -0300

    feat: add description to TodoForm

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,30 +1,46 @@
 import React, { useState } from 'react';
-
-interface TodoFormProps {
-    addTodo: (text: string) => void;
-}
+import { TodoFormProps } from '../lib/types';
 
 const TodoForm: React.FC<TodoFormProps> = ({ addTodo }) => {
-    const [text, setText] = useState('');
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        if (text.trim()) {
-            addTodo(text.trim());
-            setText('');
+        if (title.trim()) {
+            addTodo(title.trim(), description.trim());
+            setTitle('');
+            setDescription('');
         }
     };
 
     return (
-        <form onSubmit={handleSubmit} className="mb-4" data-testid="new-todo-form">
+        <form
+            onSubmit={handleSubmit}
+            className="mb-4"
+            data-testid="new-todo-form"
+        >
             <input
                 type="text"
-                className="border rounded w-full py-4 px-5 placeholder:italic"
-                placeholder="I need to do..."
-                value={text}
-				data-testid="new-todo-input"
-                onChange={(e) => setText(e.target.value)}
+                className="border rounded w-full py-3 pl-5 placeholder:italic"
+                placeholder="Title*"
+                value={title}
+                data-testid="new-todo-input"
+                onChange={(e) => setTitle(e.target.value)}
             />
+            <textarea
+                className="border rounded w-full py-4 pl-4 placeholder:italic"
+                placeholder="Description"
+                value={description}
+                data-testid="new-todo-description"
+                onChange={(e) => setDescription(e.target.value)}
+            />
+			<button
+				type="submit"
+				className="border rounded w-full py-3 bg-kGreen text-kBlack text-xl hover:bg-kDarkGreen"
+			>
+				Create
+			</button>
         </form>
     );
 };
