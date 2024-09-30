89a0c31d4567a170124efa8a31f436563a319684
lucas.quadros <1lucascq@gmail.com.br>
2024-09-30 12:13:00 -0300

    test: add tests for adding, editing, deleting, and toggling todos
    Added tests to ensure that the application correctly handles adding, editing, deleting, and toggling todos. The tests cover scenarios such as adding a new todo, editing a todo, deleting a todo, and toggling the completion status of a todo.

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,16 +1,116 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
-	render(<App />);
-	const header = screen.getByTestId(/header/i);
-	expect(header).toBeInTheDocument();
-  });
-  
 test('renders Kadince Task Manager title', () => {
     render(<App />);
     const titleElement = screen.getByText(/Kadince Task Manager/i);
     expect(titleElement).toBeInTheDocument();
 });
 
+test('renders new todo input', () => {
+    render(<App />);
+    const input = screen.getByTestId('new-todo-input');
+    expect(input).toBeInTheDocument();
+});
+
+test('adds a new todo and edit saving with Enter', async () => {
+    render(<App />);
+    const input = screen.getByTestId('new-todo-input');
+    const form = screen.getByTestId('new-todo-form');
+    fireEvent.change(input, { target: { value: 'Do dis' } });
+    fireEvent.keyDown(input, { key: 'Enter', code: 'Enter', charCode: 13 });
+    fireEvent.submit(form);
+
+    const todoList = screen.getByTestId('todo-list');
+    expect(todoList).toBeInTheDocument();
+
+    const newTodo = screen.getByTestId('todo-0');
+    expect(newTodo).toHaveTextContent('Do dis');
+
+    const editButton = screen.getByTestId('edit-todo-btn-0');
+    fireEvent.click(editButton);
+
+    const textarea = screen.getByTestId('edit-todo-textarea-0');
+    fireEvent.change(textarea, { target: { value: 'Do this' } });
+    fireEvent.keyDown(textarea, { key: 'Enter', code: 'Enter', charCode: 13 });
+
+    await waitFor(() => {
+        const updatedTodo = screen.getByTestId('todo-0');
+        expect(updatedTodo).toHaveTextContent('Do this');
+    });
+});
+test('adds a new todo and edit saving by clicking on the save btn', async () => {
+    render(<App />);
+    const input = screen.getByTestId('new-todo-input');
+    const form = screen.getByTestId('new-todo-form');
+    fireEvent.change(input, { target: { value: 'Do tat' } });
+    fireEvent.keyDown(input, { key: 'Enter', code: 'Enter', charCode: 13 });
+    fireEvent.submit(form);
+
+    const todoList = screen.getByTestId('todo-list');
+    expect(todoList).toBeInTheDocument();
+
+    const newTodo = screen.getByTestId('todo-0');
+    expect(newTodo).toHaveTextContent('Do tat');
+
+    const editButton = screen.getByTestId('edit-todo-btn-0');
+    fireEvent.click(editButton);
+
+    const textarea = screen.getByTestId('edit-todo-textarea-0');
+    fireEvent.change(textarea, { target: { value: 'Do that' } });
+    const saveButton = screen.getByTestId('save-todo-btn-0');
+    fireEvent.click(saveButton);
+
+    await waitFor(() => {
+        const updatedTodo = screen.getByTestId('todo-0');
+        expect(updatedTodo).toHaveTextContent('Do that');
+    });
+});
+
+test('deletes a todo', () => {
+    render(<App />);
+    const input = screen.getByTestId('new-todo-input');
+    const form = screen.getByTestId('new-todo-form');
+    fireEvent.change(input, { target: { value: 'To be deleted...' } });
+    fireEvent.keyDown(input, { key: 'Enter', code: 'Enter', charCode: 13 });
+    fireEvent.submit(form);
+
+    const todoList = screen.getByTestId('todo-list');
+    expect(todoList).toBeInTheDocument();
+
+    const newTodo = screen.getByTestId('todo-0');
+    expect(newTodo).toHaveTextContent('To be deleted...');
+
+    const deleteBtn = screen.getByTestId('delete-todo-btn-0');
+    fireEvent.click(deleteBtn);
+
+    expect(newTodo).not.toBeInTheDocument();
+});
+
+test('toggles a todo', () => {
+    render(<App />);
+    const input = screen.getByTestId('new-todo-input');
+    const form = screen.getByTestId('new-todo-form');
+    fireEvent.change(input, { target: { value: 'To be deleted...' } });
+    fireEvent.keyDown(input, { key: 'Enter', code: 'Enter', charCode: 13 });
+    fireEvent.submit(form);
+
+    const todoList = screen.getByTestId('todo-list');
+    expect(todoList).toBeInTheDocument();
+
+    const newTodo = screen.getByTestId('todo-0');
+    expect(newTodo).toHaveTextContent('To be deleted...');
+    expect(newTodo).not.toHaveClass('line-through');
+
+    const completeBtn = screen.getByTestId('complete-todo-btn-0');
+    fireEvent.click(completeBtn);
+
+    const completedTodo = screen.getByTestId('complete-todo-btn-0');
+    expect(completedTodo).toHaveClass('line-through');
+
+    fireEvent.click(completeBtn);
+
+    const uncompletedTodo = screen.getByTestId('complete-todo-btn-0');
+    expect(uncompletedTodo).not.toHaveClass('line-through');
+});
